7c571899c5e708f924a9ff27ef9e846cf7494783
dantheman77 <danny.wiebe@online.de>
2022-10-31 12:29:23 +0100

    Change Project Structure - moved code to note_editor.dart

--- lib/editor/note_editor.dart
+++ lib/editor/note_editor.dart
@@ -3,7 +3,44 @@ import 'package:flutter/material.dart';
 class NoteDash extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Expanded(child: TextField(
+                decoration:  InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText:'Title',
+                ),
+              ),
+              ),
+              Expanded(
+                child:TextField(
+                  decoration:  InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText:'Note',
+                  ),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                ),
+              ),
+              Expanded(child: ElevatedButton(
+                  onPressed: (){},
+                  child: const Text(
+                    'Save',
+                    style: TextStyle(fontSize: 14),
+                  )
+              ))
+            ],
+          ),
+        ),
+      ),
+
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'login/login.dart';
+
 void main() {
-  runApp(MaterialApp(home: MyApp(),),);
-}
-
-class MyApp extends StatelessWidget{
-  const MyApp ();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notes'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Expanded(child: TextField(
-                decoration:  InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText:'Title',
-                ),
-              ),
-              ),
-              Expanded(
-                child:TextField(
-                  decoration:  InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText:'Note',
-                  ),
-                  keyboardType: TextInputType.multiline,
-                  maxLines: null,
-                ),
-              ),
-              Expanded(child: ElevatedButton(
-                onPressed: (){},
-                child: const Text(
-                  'Save',
-                  style: TextStyle(fontSize: 14),
-                )
-              ))
-            ],
-          ),
-        ),
-      ),
-
-    );
-  }
-
-
-
-
+  runApp(MaterialApp(home: NoteLogin()));
 }
