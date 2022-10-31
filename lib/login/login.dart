08135fa345b4b2272720c890befed8da956270fa
dantheman77 <danny.wiebe@online.de>
2022-10-31 12:27:04 +0100

    Change Project Structure

---
+++ lib/login/login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoteLogin extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
